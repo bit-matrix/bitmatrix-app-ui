a41c778be4aebb58ede8f49f690c94a0f566ea71
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-05-26 01:30:44 +0300

    WalletListModal install marina wallet loading manage

--- src/components/WalletListModal/WalletListModal.tsx
+++ src/components/WalletListModal/WalletListModal.tsx
@@ -13,93 +13,93 @@ import ledgerNano from "../../images/ledger.png";
 import generateSeed from "../../images/key_2.png";
 
 interface IWalletListModal {
-  show: boolean;
-  walletOnClick: (walletName: WALLET_NAME) => void;
-  close: () => void;
-  setNewAddress: (newAddress: MarinaAddressInterface) => void;
-  setUtxos: (utxos: UtxoInterface[]) => void;
+    show: boolean;
+    walletOnClick: (walletName: WALLET_NAME) => void;
+    close: () => void;
+    setNewAddress: (newAddress: MarinaAddressInterface) => void;
+    setUtxos: (utxos: UtxoInterface[]) => void;
 }
 
 const WalletListModal: React.FC<IWalletListModal> = ({ show, walletOnClick, close, setNewAddress, setUtxos }) => {
-  const [wallet, setWallet] = useState<IWallet>();
-  const [loading, setLoading] = useState<boolean>(false);
+    const [wallet, setWallet] = useState<IWallet>();
+    const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (show) {
-      setWallet(new Wallet(WALLET_NAME.MARINA));
-    }
-  }, [show]);
+    useEffect(() => {
+        if (show) {
+            setWallet(new Wallet(WALLET_NAME.MARINA));
+        }
+    }, [show]);
 
-  const connectWalletOnClick = (): void => {
-    wallet?.exist()
-      ? wallet.enable().then(() => {
-          console.log("successLogin");
+    const connectWalletOnClick = (): void => {
+        wallet?.exist()
+            ? wallet.enable().then(() => {
+                  console.log("successLogin");
 
-          // wallet.getNextAddress().then((newAddress: MarinaAddressInterface) => {
-          // setNewAddress(newAddress);
+                  // wallet.getNextAddress().then((newAddress: MarinaAddressInterface) => {
+                  // setNewAddress(newAddress);
 
-          wallet.getAddresses().then((addresses) => {
-            console.log("addresses", addresses);
+                  wallet.getAddresses().then(addresses => {
+                      console.log("addresses", addresses);
 
-            fetchUTXOS(addresses).then((utxos) => {
-              console.log("UTXOS", utxos);
-              setUtxos(utxos);
-              setLoading(false);
-              close();
-            });
-          });
-          //});
-        })
-      : window.open("https://chrome.google.com/webstore/detail/marina/nhanebedohgejbllffboeipobccgedhl/related");
-  };
+                      fetchUTXOS(addresses).then(utxos => {
+                          console.log("UTXOS", utxos);
+                          setUtxos(utxos);
+                          setLoading(false);
+                          close();
+                      });
+                  });
+                  //});
+              })
+            : window.open("https://chrome.google.com/webstore/detail/marina/nhanebedohgejbllffboeipobccgedhl/related");
+    };
 
-  return (
-    <Modal className="wallet-list-modal" size="xs" backdrop={true} show={show} onHide={close}>
-      {loading ? <Loader className="wallet-list-loading" inverse center /> : null}
-      <Modal.Header>
-        <Modal.Title>Connect Wallet</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div
-          className="rs-panel rs-panel-default rs-panel-body wallet-list-item"
-          onClick={() => {
-            setLoading(true);
-            connectWalletOnClick();
-          }}
-        >
-          <img className="wallet-list-item-icon" src={marinaWallet} alt="Generate Seed" />
-          <div>
-            <div className="wallet-text-header">{wallet?.exist() ? "MarinaWallet" : "Install MarinaWallet"} </div>
-            <div className="wallet-list-item-description"> Connect to Marina wallet to access Bitmatrix directly from your browser.</div>
-          </div>
-        </div>
+    return (
+        <Modal className="wallet-list-modal" size="xs" backdrop={true} show={show} onHide={close}>
+            {loading ? <Loader className="wallet-list-loading" inverse center /> : null}
+            <Modal.Header>
+                <Modal.Title>Connect Wallet</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div
+                    className="rs-panel rs-panel-default rs-panel-body wallet-list-item"
+                    onClick={() => {
+                        if (wallet?.exist()) setLoading(true);
+                        connectWalletOnClick();
+                    }}
+                >
+                    <img className="wallet-list-item-icon" src={marinaWallet} alt="Generate Seed" />
+                    <div>
+                        <div className="wallet-text-header">{wallet?.exist() ? "MarinaWallet" : "Install MarinaWallet"} </div>
+                        <div className="wallet-list-item-description"> Connect to Marina wallet to access Bitmatrix directly from your browser.</div>
+                    </div>
+                </div>
 
-        <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
-          <img className="wallet-list-item-icon" src={ledgerNano} alt="Import Seed" />
-          <div>
-            <div className="wallet-text-header">Ledger Nano</div>
-            <div className="wallet-list-item-description">Connect to your Ledger Nano device to access to Bitmatrix.</div>
-          </div>
-        </div>
+                <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
+                    <img className="wallet-list-item-icon" src={ledgerNano} alt="Import Seed" />
+                    <div>
+                        <div className="wallet-text-header">Ledger Nano</div>
+                        <div className="wallet-list-item-description">Connect to your Ledger Nano device to access to Bitmatrix.</div>
+                    </div>
+                </div>
 
-        <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
-          <img className="wallet-list-item-icon" src={importSeed} alt="Import Seed" />
-          <div>
-            <div className="wallet-text-header">Import Seed</div>
-            <div className="wallet-list-item-description">Import your 12-word mnemonic phrases to unlock your coins and start swapping.</div>
-          </div>
-        </div>
+                <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
+                    <img className="wallet-list-item-icon" src={importSeed} alt="Import Seed" />
+                    <div>
+                        <div className="wallet-text-header">Import Seed</div>
+                        <div className="wallet-list-item-description">Import your 12-word mnemonic phrases to unlock your coins and start swapping.</div>
+                    </div>
+                </div>
 
-        <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
-          <img className="wallet-list-item-icon" src={generateSeed} alt="Generate Seed" />
-          <div>
-            <div className="wallet-text-header">Generate Seed</div>
-            <div className="wallet-list-item-description">Generate new 12-word mnemonic phrases and get strated with Bitmatri</div>
-          </div>
-        </div>
-      </Modal.Body>
-    </Modal>
-  );
+                <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
+                    <img className="wallet-list-item-icon" src={generateSeed} alt="Generate Seed" />
+                    <div>
+                        <div className="wallet-text-header">Generate Seed</div>
+                        <div className="wallet-list-item-description">Generate new 12-word mnemonic phrases and get strated with Bitmatri</div>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+    );
 };
 
 export default WalletListModal;
