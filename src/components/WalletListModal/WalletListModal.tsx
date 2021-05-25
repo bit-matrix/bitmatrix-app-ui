47a5ac417236e69a0cb119376b9823f9f8f629ae
afarukcali <afarukcali@gmail.com>
2021-05-25 23:58:08 +0300

    WalletListModal loader added

--- src/components/WalletListModal/WalletListModal.tsx
+++ src/components/WalletListModal/WalletListModal.tsx
@@ -9,80 +9,75 @@ import { UtxoInterface } from "ldk";
 import "./WalletListModal.scss";
 
 interface IWalletListModal {
-    show: boolean;
-    walletOnClick: (walletName: WALLET_NAME) => void;
-    close: () => void;
-    setNewAddress: (newAddress: MarinaAddressInterface) => void;
-    setUtxos: (utxos: UtxoInterface[]) => void;
+  show: boolean;
+  walletOnClick: (walletName: WALLET_NAME) => void;
+  close: () => void;
+  setNewAddress: (newAddress: MarinaAddressInterface) => void;
+  setUtxos: (utxos: UtxoInterface[]) => void;
 }
 
 const WalletListModal: React.FC<IWalletListModal> = ({ show, walletOnClick, close, setNewAddress, setUtxos }) => {
-    const [wallet, setWallet] = useState<IWallet>();
-    const [loading, setLoading] = useState<boolean>(false);
+  const [wallet, setWallet] = useState<IWallet>();
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        if (show) {
-            setWallet(new Wallet(WALLET_NAME.MARINA));
-        }
-    }, [show]);
+  useEffect(() => {
+    if (show) {
+      setWallet(new Wallet(WALLET_NAME.MARINA));
+    }
+  }, [show]);
 
-    const connectWalletOnClick = (): void => {
-        wallet?.exist()
-            ? wallet.enable().then(() => {
-                  console.log("successLogin");
+  const connectWalletOnClick = (): void => {
+    wallet?.exist()
+      ? wallet.enable().then(() => {
+          console.log("successLogin");
 
-                  wallet.getNextAddress().then((newAddress: MarinaAddressInterface) => {
-                      setNewAddress(newAddress);
+          wallet.getNextAddress().then((newAddress: MarinaAddressInterface) => {
+            setNewAddress(newAddress);
 
-                      wallet.getAddresses().then(addresses => {
-                          console.log("addresses", addresses);
+            wallet.getAddresses().then((addresses) => {
+              console.log("addresses", addresses);
 
-                          fetchUTXOS(addresses).then(utxos => {
-                              console.log("UTXOS", utxos);
-                              setUtxos(utxos);
-                              setLoading(false);
-                              close();
-                          });
-                      });
-                  });
-              })
-            : window.open("https://chrome.google.com/webstore/detail/marina/nhanebedohgejbllffboeipobccgedhl/related");
-    };
+              fetchUTXOS(addresses).then((utxos) => {
+                console.log("UTXOS", utxos);
+                setUtxos(utxos);
+                setLoading(false);
+                close();
+              });
+            });
+          });
+        })
+      : window.open("https://chrome.google.com/webstore/detail/marina/nhanebedohgejbllffboeipobccgedhl/related");
+  };
 
-    return (
-        <Modal className="wallet-list-modal" size="xs" backdrop={true} show={show} onHide={close}>
-            {loading ? (
-                <div className="wallet-list-loading">
-                    <Loader size="sm" />
-                </div>
-            ) : null}
-
-            <Modal.Header>
-                <Modal.Title>Connect Wallet</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div
-                    className="rs-panel rs-panel-default rs-panel-body wallet-list-item"
-                    onClick={() => {
-                        setLoading(true);
-                        connectWalletOnClick();
-                    }}
-                >
-                    <span className="wallet-text-header">MarinaWallet</span>
-                    {/* <span>
+  return (
+    <Modal className="wallet-list-modal" size="xs" backdrop={true} show={show} onHide={close}>
+      {loading ? <Loader className="wallet-list-loading" inverse center /> : null}
+      <Modal.Header>
+        <Modal.Title>Connect Wallet</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div
+          className="rs-panel rs-panel-default rs-panel-body wallet-list-item"
+          onClick={() => {
+            setLoading(true);
+            connectWalletOnClick();
+          }}
+        >
+          <span className="wallet-text-header">MarinaWallet</span>
+          {/* <span>
         Connect to Marina wallet to access Bitmatrix
         <br /> directly from your browser
       </span> */}
-                </div>
-                <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
-                    <span className="wallet-text-header">Import Seed</span>
-                </div>
-                <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
-                    <span className="wallet-text-header">Generate Seed</span>
-                </div>
-            </Modal.Body>
-        </Modal>
-    );
+        </div>
+        <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
+          <span className="wallet-text-header">Import Seed</span>
+        </div>
+        <div className="wallet-list-item rs-panel rs-panel-default rs-panel-body disabled">
+          <span className="wallet-text-header">Generate Seed</span>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default WalletListModal;
