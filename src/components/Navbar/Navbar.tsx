38ff22486754f693fe8691a56ecb5e374624c9ed
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-06-24 21:49:34 +0300

    Navbar: useHistory added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,64 @@
 import React from 'react';
 import bitmatrix_icon from '../../images/bitmatrix_icon.png';
 import { Button } from 'rsuite';
+import { useHistory } from 'react-router';
+import { ROUTE_PATH } from '../../enum/ROUTE_PATH';
 import './Navbar.scss';
 
 export const Navbar = (): JSX.Element => {
+  const history = useHistory();
+
   return (
     <ul className="navbar-main">
       <li className="navbar-item">
-        <Button className="navbar-home-button ml-5">
+        <Button
+          className="navbar-home-button ml-5"
+          onClick={() => {
+            history.push(ROUTE_PATH.HOME);
+          }}
+        >
           <img className="navbar-home-icon" src={bitmatrix_icon} alt="" />
         </Button>
       </li>
       <li className="navbar-item">
-        <Button className="navbar-item-button">Swap</Button>
+        <Button
+          className="navbar-item-button"
+          onClick={() => {
+            history.push(ROUTE_PATH.SWAP);
+          }}
+        >
+          Swap
+        </Button>
       </li>
       <li className="navbar-item">
-        <Button className="navbar-item-button">Pool</Button>
+        <Button
+          className="navbar-item-button"
+          // onClick={() => {
+          //   history.push(ROUTE_PATH.POOL);
+          // }}
+        >
+          Pool
+        </Button>
       </li>
       <li className="navbar-item">
-        <Button className="navbar-item-button">Stats</Button>
+        <Button
+          className="navbar-item-button"
+          // onClick={() => {
+          //   history.push(ROUTE_PATH.STATS);
+          // }}
+        >
+          Stats
+        </Button>
       </li>
       <li className="navbar-item mobile-hidden">
-        <Button className="navbar-item-button mr-5">Settings</Button>
+        <Button
+          className="navbar-item-button mr-5"
+          // onClick={() => {
+          //   history.push(ROUTE_PATH.SETTINGS);
+          // }}
+        >
+          Settings
+        </Button>
       </li>
     </ul>
   );
