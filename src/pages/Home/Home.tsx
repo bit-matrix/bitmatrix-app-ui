e778ae0fd70a2333c41c07f55671a3d9ca265c16
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-06-25 00:06:43 +0300

    Home: Info component used.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
 import { Button } from 'rsuite';
+import { useHistory } from 'react-router-dom';
 import './Home.scss';
+import { ROUTE_PATH } from '../../enum/ROUTE_PATH';
+import { Info } from '../../components/common/Info/Info';
 
 export const Home = (): JSX.Element => {
+  const history = useHistory();
   return (
-    <div className="home-page-main">
-      <div className="home-page-desc">
-        <h3 className="home-page-desc-header">
-          Anonymous liquidity provision here!
-        </h3>
-        <div className="home-page-desc-content">
-          <p>
-            Bitmatrix is a constant product market maker built on Liquid
-            Network.
-          </p>
-          <p>
-            Bitmatrix utilizes the power of bitcoin opcodes to enable automated
-            liquidity provision across L-BTC and other Liquid-based assets.
-          </p>
-          <p>
-            Bitmatrix comes with maximally Uniswap-like UX and is effectively
-            powered by Liquid's powerfull scripting capatilies.
-          </p>
+    <div>
+      <div className="home-page-main">
+        <div className="home-page-desc">
+          <h3 className="home-page-desc-header">
+            Anonymous liquidity provision here!
+          </h3>
+          <div className="home-page-desc-content">
+            <p>
+              Bitmatrix is a constant product market maker built on Liquid
+              Network.
+            </p>
+            <p>
+              Bitmatrix utilizes the power of bitcoin opcodes to enable
+              automated liquidity provision across L-BTC and other Liquid-based
+              assets.
+            </p>
+            <p>
+              Bitmatrix comes with maximally Uniswap-like UX and is effectively
+              powered by Liquid's powerfull scripting capatilies.
+            </p>
+          </div>
+          <Button
+            className="home-page-start-button"
+            appearance="default"
+            onClick={() => {
+              history.push(ROUTE_PATH.SWAP);
+            }}
+          >
+            Start Swapping
+          </Button>
         </div>
-        <Button className="home-page-start-button" appearance="default">
-          Start Swapping
-        </Button>
+        <div className="home-page-img"></div>
       </div>
-      <div className="home-page-img"></div>
+      <Info
+        content="Always make sure your URL is 'bitmatrix.app'"
+        showCloseIcon
+      />
     </div>
   );
 };
