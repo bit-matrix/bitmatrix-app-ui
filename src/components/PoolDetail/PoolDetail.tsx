af1837e8e1ba675136e7a38204ff2e300fc026f4
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-07-01 23:52:42 +0300

    PoolDetail: PoolDetail component UI

--- src/components/PoolDetail/PoolDetail.tsx
+++ src/components/PoolDetail/PoolDetail.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { Icon, IconButton, Nav } from 'rsuite';
+import { Button, Icon, Nav } from 'rsuite';
 import { PoolData } from '../../model/PoolData';
+import lbtcImage from '../../images/liquid_btc.png';
+import usdtImage from '../../images/usdt.png';
+import './PoolDetail.scss';
 
 enum PoolDetailTabs {
   PRICE = 'price',
@@ -20,32 +23,95 @@ export const PoolDetail: React.FC<Props> = ({ back }) => {
   );
 
   return (
-    <div className="pool-detail-page-main">
-      <div className="pool-page-header">
-        <IconButton
-          className="pool-detail-page-button"
-          onClick={back}
-          icon={
+    <div className="pool-detail-main">
+      <div className="pool-detail-header">
+        <div className="pool-detail-header-left">
+          <a className="pool-detail-button" onClick={back}>
             <Icon
-              className="pool-detail-page-icon"
+              className="pool-detail-back-icon"
               icon="angle-left"
               size="4x"
             />
-          }
-        />
+          </a>
+          <div className="pool-detail-page-text">L-BTC/USDT</div>
+        </div>
         <Nav
           activeKey={selectedTab}
           onSelect={(eventKey: any) => setSelectedTab(eventKey)}
-          className="pool-detail-page-tabs"
+          className="pool-detail-tabs"
           appearance="subtle"
         >
-          <Nav.Item eventKey={PoolDetailTabs.PRICE}>Price</Nav.Item>
-          <Nav.Item eventKey={PoolDetailTabs.VOLUME}>Volume</Nav.Item>
-          <Nav.Item eventKey={PoolDetailTabs.LIQUIDITY}>Liquidity</Nav.Item>
-          <Nav.Item eventKey={PoolDetailTabs.FEES}>Fees</Nav.Item>
+          <Nav.Item
+            className="pool-detail-navbar-item"
+            eventKey={PoolDetailTabs.PRICE}
+          >
+            Price
+          </Nav.Item>
+          <Nav.Item
+            className="pool-detail-navbar-item"
+            eventKey={PoolDetailTabs.VOLUME}
+          >
+            Volume
+          </Nav.Item>
+          <Nav.Item
+            className="pool-detail-navbar-item"
+            eventKey={PoolDetailTabs.LIQUIDITY}
+          >
+            Liquidity
+          </Nav.Item>
+          <Nav.Item
+            className="pool-detail-navbar-item"
+            eventKey={PoolDetailTabs.FEES}
+          >
+            Fees
+          </Nav.Item>
         </Nav>
       </div>
-      <div className="pool-detail-page-content">detay</div>
+      <div className="pool-detail-content">
+        <div className="pool-detail-content-left">
+          <div>Pool Pairs</div>
+          <div className="pool-detail-item">
+            <div className="pool-detail-img-content">
+              <img className="pool-detail-img" src={lbtcImage} alt="" />
+              <span>L-BTC</span>
+            </div>
+            82.54m
+          </div>
+
+          <div className="pool-detail-item">
+            <div className="pool-detail-img-content">
+              <img className="pool-detail-img" src={usdtImage} alt="" />
+              <span>USDT</span>
+            </div>
+            123.41k
+          </div>
+
+          <div className="pool-detail-volume-fee">
+            <div>
+              <div>Price</div>
+              <div>%12</div>
+              <div>1.35%</div>
+            </div>
+
+            <div>
+              <div>Volume 24h</div>
+              <div>%219.20m</div>
+              <div>74.53%</div>
+            </div>
+          </div>
+
+          <Button
+            appearance="default"
+            className="primary-button"
+            onClick={() => {
+              console.log('add liquidity');
+            }}
+          >
+            Add Liquidity
+          </Button>
+        </div>
+        <div className="pool-detail-content-right"></div>
+      </div>
     </div>
   );
 };
