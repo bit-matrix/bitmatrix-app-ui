bb3c0d20e119926cfb6e67ce2ee7fd2d62c3be28
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-02 14:39:49 +0300

    Settings:Settings component created

---
+++ src/pages/Settings/Settings.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { TabMenu } from '../../components/TabMenu/TabMenu';
+import { General } from './General/General';
+import './Settings.scss';
+
+enum SettingsDetailTabs {
+  GENERAL = 'General',
+  PRIVACY = 'Privacy',
+  THEME = 'Theme',
+  ADVANCED = 'Advanced',
+}
+
+export const Settings = (): JSX.Element => {
+  const [selectedTab, setSelectedTab] = useState<SettingsDetailTabs>(
+    SettingsDetailTabs.GENERAL,
+  );
+
+  const getSettingsData = () => {
+    switch (selectedTab) {
+      case SettingsDetailTabs.GENERAL:
+        return <General />;
+        break;
+      case SettingsDetailTabs.PRIVACY:
+        return <div></div>;
+        break;
+      case SettingsDetailTabs.THEME:
+        return <div></div>;
+        break;
+      case SettingsDetailTabs.ADVANCED:
+        return <div></div>;
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <div className="settings-page-main">
+      <div className="settings-page-header">
+        <TabMenu
+          menuItems={[
+            SettingsDetailTabs.GENERAL,
+            SettingsDetailTabs.PRIVACY,
+            SettingsDetailTabs.THEME,
+            SettingsDetailTabs.ADVANCED,
+          ]}
+          selectedItem={selectedTab}
+          onClick={(eventKey: any) => setSelectedTab(eventKey)}
+        />
+      </div>
+      <div className="settings-page-content">{getSettingsData()}</div>
+    </div>
+  );
+};
