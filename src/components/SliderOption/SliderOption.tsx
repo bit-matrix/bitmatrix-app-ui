505404a93a164d324fea5b056b7c2c983db52780
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-03 13:55:11 +0300

    SliderOption: state and style

--- src/components/SliderOption/SliderOption.tsx
+++ src/components/SliderOption/SliderOption.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SliderOption.scss';
 
 type Props = {
@@ -6,12 +6,20 @@ type Props = {
 };
 
 export const SliderOption: React.FC<Props> = ({ options }) => {
+  const [value, setValue] = useState<string>(options[0]);
+
   return (
-    <ul className="slider">
+    <ul className="slider-option-main">
       {options.map((option) => {
         return (
-          <li className="slider-item">
-            <input type="radio" id="f-option" name="rb" />
+          <li className="slider-option-item">
+            <input
+              type="radio"
+              value={value}
+              onChange={(event) => setValue(event.target.value)}
+              id="f-option"
+              name="rb"
+            />
             <label className="slider-option-label" htmlFor="f-option">
               {option}
             </label>
