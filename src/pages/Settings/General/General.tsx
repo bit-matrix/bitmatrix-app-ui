116636537a7cc5378e988c58c8fddb58de945ee7
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-03 13:55:37 +0300

    General: child components added and improved

--- src/pages/Settings/General/General.tsx
+++ src/pages/Settings/General/General.tsx
@@ -8,33 +8,33 @@ import './General.scss';
 export const General = (): JSX.Element => {
   return (
     <div>
-      <div>
+      <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Slippage & Fee</span>
           <img className="general-icon" src={info} alt="info" />
         </div>
-        {/* <SliderOption options={['%0.5', '%0.75', '%1', '%1.25', '%1.5']} /> */}
+        <SliderOption options={['%0.5', '%0.75', '%1', '%1.25', '%1.5']} />
       </div>
-      <div>
+      <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Preferred Unit</span>
           <img className="general-icon" src={info} alt="info" />
         </div>
-        <div>
+        <div className="prefferred-unit">
           <CheckBox title="L-BTC" />
           <CheckBox title="mBTC" />
           <CheckBox title="µBTC" />
           <CheckBox title="SAT" />
         </div>
       </div>
-      <div>
+      <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Push Notifications</span>
           <img className="general-icon" src={info} alt="info" />
           <ToggleSwitch />
         </div>
       </div>
-      <div>
+      <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Liquid Taxi</span>
           <img className="general-icon" src={info} alt="info" />
