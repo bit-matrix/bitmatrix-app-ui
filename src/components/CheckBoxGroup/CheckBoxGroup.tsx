5c5ce91c9c2cd6eabdffee08c787e89b65c24735
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-06 16:11:30 +0300

    CheckBoxGroup: custom CheckBoxGroup component created

---
+++ src/components/CheckBoxGroup/CheckBoxGroup.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { CheckBox } from '../CheckBox/CheckBox';
+import './CheckBoxGroup.scss';
+
+type Props = {
+  options: Array<string>;
+  onChange: (checkedValues: string[]) => void;
+  checkedValues: Array<string>;
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+export const CheckBoxGroup: React.FC<Props> = ({
+  options,
+  onChange,
+  checkedValues,
+  className,
+  style,
+}) => {
+  const onChangeValue = (option: string, checked: boolean): void => {
+    const lastCheckedValues = [...checkedValues];
+    if (checked) {
+      const selectedValueIndex = lastCheckedValues.findIndex(
+        (checkedvalue) => checkedvalue === option,
+      );
+      lastCheckedValues.splice(selectedValueIndex, 1);
+    } else {
+      lastCheckedValues.push(option);
+    }
+    if (lastCheckedValues != undefined) {
+      onChange(lastCheckedValues);
+    }
+  };
+
+  return (
+    <div style={style} className={`checkbox-group-main ${className}`}>
+      {options.map((option: string, index: number) => {
+        const isSelected = checkedValues
+          ? checkedValues.includes(option)
+          : false;
+
+        return (
+          <CheckBox
+            key={index}
+            option={option}
+            checked={isSelected}
+            onChange={onChangeValue}
+          />
+        );
+      })}
+    </div>
+  );
+};
