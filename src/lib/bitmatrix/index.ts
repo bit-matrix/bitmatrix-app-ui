83d753237075fae14b01c1192c36a789b79b6c5b
afarukcali <afarukcali@gmail.com>
2021-12-09 16:19:42 +0300

    usdtToLbtcSwapAmountCalculate

--- src/lib/bitmatrix/index.ts
+++ src/lib/bitmatrix/index.ts
@@ -49,3 +49,44 @@ export const lbtcToUsdtSwapAmountCalculate = (
 
   return usdtAmount;
 };
+
+export const usdtToLbtcSwapAmountCalculate = (
+  usdtAmount: number,
+  slippage?: number,
+): number => {
+  // validation
+  if (usdtAmount < 50000000)
+    console.log('Usdt amount must greaten or at least minimum equal 50000000');
+
+  // step1 (fee calculation)
+  const lpFee = div(usdtAmount, 400);
+
+  // step2 (input new value without fee  input - step1)
+  const usdtAmountWithoutFee = usdtAmount - lpFee;
+
+  // step3 (total usd pool amount poolUsdtLiquidity + step2)
+  const totalUsdtLiquidity = poolUsdtLiquidity + usdtAmountWithoutFee;
+
+  // step4  (usdt Liquidty rate calculation step3 % 2mn)
+  const usdtLiquidtyRate = div(totalUsdtLiquidity, 2000000);
+
+  // step5 (Pool L-BTC liquidity % 16)
+  const x = div(poolLbtcLiquidity, 16);
+
+  // step6 (Pool USDT liquidity % 2MN)
+  const y = div(poolUsdtLiquidity, 2000000);
+
+  // step 7 (constant x*y = k step5*step6)
+  const constant = x * y;
+
+  // step 8 (constant * usdtLiquidtyRate  step7*step4
+  const constantRate = div(constant, usdtLiquidtyRate);
+
+  //step 9 (step 8 * 16)
+  const lbtcAmount = constantRate * 16;
+
+  //step 10 (poolLbtcLiquidity - step9)
+  const remainingLbtcAmount = poolLbtcLiquidity - lbtcAmount;
+
+  return remainingLbtcAmount;
+};
