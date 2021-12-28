608c08e6abc65a4dcf048cdeb295f543ac2f4253
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-28 13:37:05 +0300

    reducer: case SETTINGS_ACTION_TYPES.SET_PREFERRED_UNIT:

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -1,9 +1,13 @@
+import { PREFERRED_UNIT } from '../enum/PREFERRED_UNIT';
 import PayloadData from './PayloadData';
 import SETTINGS_ACTION_TYPES from './SETTINGS_ACTION_TYPES';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const reducer = (
-  state: PayloadData = { slippage: 200 },
+  state: PayloadData = {
+    slippage: 200,
+    preferred_unit: { text: PREFERRED_UNIT.SAT, value: 1 },
+  },
   action: {
     type: SETTINGS_ACTION_TYPES;
     payload: PayloadData;
@@ -14,6 +18,12 @@ const reducer = (
       return {
         ...state,
         slippage: action.payload.slippage,
+      };
+
+    case SETTINGS_ACTION_TYPES.SET_PREFERRED_UNIT:
+      return {
+        ...state,
+        unit: action.payload.preferred_unit,
       };
 
     default:
