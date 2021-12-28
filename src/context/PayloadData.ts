cd5771c8dfd2f68047a4bf13740d00a4da427fea
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-28 13:36:47 +0300

    PayloadData : preferred_unit

--- src/context/PayloadData.ts
+++ src/context/PayloadData.ts
@@ -1,5 +1,8 @@
+import { PREFERRED_UNIT } from '../enum/PREFERRED_UNIT';
+
 interface PayloadData {
   slippage: number;
+  preferred_unit: { text: PREFERRED_UNIT; value: number };
 }
 
 export default PayloadData;
