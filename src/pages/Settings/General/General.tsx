b6e7fe97b7993fa135d0d5dd1a0e213170103ba1
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-28 13:38:26 +0300

    General: preferren unit managed

--- src/pages/Settings/General/General.tsx
+++ src/pages/Settings/General/General.tsx
@@ -1,60 +1,21 @@
 import React, { useState } from 'react';
 import { useContext } from 'react';
+import SETTINGS_ACTION_TYPES from '../../../context/SETTINGS_ACTION_TYPES';
+import SettingsContext from '../../../context/SettingsContext';
 import { CheckBoxGroup } from '../../../components/CheckBoxGroup/CheckBoxGroup';
 import { StripedRadioButton } from '../../../components/StripedRadioButton/StripedRadioButton';
 import { ToggleSwitch } from '../../../components/ToggleSwitch/ToggleSwitch';
-import SettingsContext from '../../../context/SettingsContext';
-import SETTINGS_ACTION_TYPES from '../../../context/SETTINGS_ACTION_TYPES';
 import { PREFERRED_UNIT } from '../../../enum/PREFERRED_UNIT';
-import { SLIPPAGE_FEE } from '../../../enum/SLIPPAGE_FEE';
+import {
+  preferredUnitList,
+  preferredUnitOptions,
+  SlippageFeeList,
+  slippageFeeOptions,
+} from './utils';
 import info from '../../../images/info2.png';
 import './General.scss';
 
-const preferredUnitOptions = [
-  PREFERRED_UNIT.LBTC,
-  PREFERRED_UNIT.mBTC,
-  PREFERRED_UNIT.uBTC,
-  PREFERRED_UNIT.SAT,
-];
-
-const slippageFeeOptions = [
-  SLIPPAGE_FEE.ZEROPOINTTHREE,
-  SLIPPAGE_FEE.ZEROPOINTFIVE,
-  SLIPPAGE_FEE.ZEROPOINTSEVENTYFIVE,
-  SLIPPAGE_FEE.ONEPOINT,
-  SLIPPAGE_FEE.ONEPOINTTWENTYFIVE,
-  SLIPPAGE_FEE.ONEPOINTFIVE,
-];
-
-const SlippageFeeList = [
-  {
-    text: SLIPPAGE_FEE.ZEROPOINTFIVE,
-    value: 333,
-  },
-  {
-    text: SLIPPAGE_FEE.ZEROPOINTFIVE,
-    value: 200,
-  },
-  {
-    text: SLIPPAGE_FEE.ZEROPOINTSEVENTYFIVE,
-    value: 133,
-  },
-  {
-    text: SLIPPAGE_FEE.ONEPOINT,
-    value: 100,
-  },
-  {
-    text: SLIPPAGE_FEE.ONEPOINTTWENTYFIVE,
-    value: 80,
-  },
-  {
-    text: SLIPPAGE_FEE.ONEPOINTFIVE,
-    value: 66,
-  },
-];
-
 export const General = (): JSX.Element => {
-  const [checkedValue, setCheckedValue] = useState<PREFERRED_UNIT>();
   const [pushNotificationsSwitch, setPushNotificationsSwitch] =
     useState<boolean>(false);
   const [liquidTaxiSwitch, setLiquidTaxiSwitch] = useState<boolean>(false);
@@ -80,7 +41,10 @@ export const General = (): JSX.Element => {
 
             dispatch({
               type: SETTINGS_ACTION_TYPES.SET_SLIPPAGE,
-              payload: { slippage: selectedoOption || 200 },
+              payload: {
+                slippage: selectedoOption || 200,
+                preferred_unit: payloadData.preferred_unit,
+              },
             });
           }}
         />
@@ -95,9 +59,20 @@ export const General = (): JSX.Element => {
           className="prefferred-unit"
           options={preferredUnitOptions}
           onChange={(checkedValue: PREFERRED_UNIT | undefined) => {
-            setCheckedValue(checkedValue);
+            const preferredUnit = preferredUnitList.find(
+              (unit) => unit.text === checkedValue,
+            );
+            if (preferredUnit) {
+              dispatch({
+                type: SETTINGS_ACTION_TYPES.SET_PREFERRED_UNIT,
+                payload: {
+                  slippage: payloadData.slippage,
+                  preferred_unit: preferredUnit,
+                },
+              });
+            }
           }}
-          checkedValue={checkedValue}
+          checkedValue={payloadData.preferred_unit.text}
         />
       </div>
       <div className="general-item">
