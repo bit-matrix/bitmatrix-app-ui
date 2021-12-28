d58109e6762ebebac49073a90f9e4d06bcf44fb6
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-28 13:37:45 +0300

    SettingsProvider: preferred_unit

--- src/context/SettingsProvider.tsx
+++ src/context/SettingsProvider.tsx
@@ -1,9 +1,13 @@
 import React, { useReducer } from 'react';
+import { PREFERRED_UNIT } from '../enum/PREFERRED_UNIT';
 import reducer from './reducer';
 import SettingsContext from './SettingsContext';
 
 const SettingsProvider: React.FC = ({ children }) => {
-  const [payloadData, dispatch] = useReducer(reducer, { slippage: 200 });
+  const [payloadData, dispatch] = useReducer(reducer, {
+    slippage: 200,
+    preferred_unit: { text: PREFERRED_UNIT.SAT, value: 1 },
+  });
 
   return (
     <SettingsContext.Provider value={{ payloadData, dispatch }}>
