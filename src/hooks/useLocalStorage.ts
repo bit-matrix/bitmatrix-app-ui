e1b5f83d811998802e107f95fdd754fb1ed6df32
afarukcali <afarukcali@gmail.com>
2021-12-29 16:11:04 +0300

    localStorage hooks review.

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,23 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { useState } from 'react';
 
 export const useLocalStorage = <T>(k: string) => {
-  const value = localStorage.getItem(k);
-  const object = value !== null ? JSON.parse(value) : undefined;
-  const [data, setData] = useState<T | undefined>(object);
-
   const getTxLocalData = (): T | undefined => {
-    return data;
+    const value = localStorage.getItem(k);
+    return value !== null ? JSON.parse(value) : undefined;
   };
 
   const setTxLocalData = (value: T) => {
-    setData(value);
     localStorage.setItem(k, JSON.stringify(value));
   };
 
   const clearTxLocalData = () => {
     localStorage.removeItem(k);
-    setData(undefined);
   };
 
   return { getTxLocalData, setTxLocalData, clearTxLocalData };
