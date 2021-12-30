ce82fee32c65552ea699758a3edf537a417755a9
afarukcali <afarukcali@gmail.com>
2021-12-30 15:29:15 +0300

    Wallet on click event.

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -42,6 +42,8 @@ export const Swap = (): JSX.Element => {
 
   const [wallet, setWallet] = useState<IWallet>();
 
+  const [selectedWalletName, setSelectedWalletName] = useState<WALLET_NAME>();
+
   const [walletIsEnabled, setWalletIsEnabled] = useState<boolean>(false);
 
   const [poolConfigs, setPoolConfigs] = useState<BmConfig>();
@@ -54,9 +56,11 @@ export const Swap = (): JSX.Element => {
 
   // connect marina
   useEffect(() => {
+    console.log(selectedWalletName);
     detectProvider('marina')
       .then((marina) => {
-        const marinaWallet = new Wallet(WALLET_NAME.MARINA);
+        console.log('then');
+        const marinaWallet = new Wallet(selectedWalletName);
         setWallet(marinaWallet);
 
         marina.isEnabled().then((enabled) => {
@@ -64,10 +68,10 @@ export const Swap = (): JSX.Element => {
         });
       })
       .catch(() => {
-        const marinaWallet = new Wallet(WALLET_NAME.MARINA);
+        const marinaWallet = new Wallet(selectedWalletName);
         setWallet(marinaWallet);
       });
-  }, [walletIsEnabled]);
+  }, [walletIsEnabled, selectedWalletName]);
 
   // fetch pool config
   useEffect(() => {
@@ -278,7 +282,7 @@ export const Swap = (): JSX.Element => {
       <WalletListModal
         show={showWalletList}
         wallet={wallet}
-        walletOnClick={(walletName: WALLET_NAME) => setWallet(new Wallet(walletName))}
+        walletOnClick={(walletName: WALLET_NAME) => setSelectedWalletName(walletName)}
         close={() => setShowWalletList(false)}
         // setNewAddress={setNewAddress}
         setUtxos={setUtxosAll}
