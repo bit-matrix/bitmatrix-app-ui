c616f40a48aaa4ab5b86fc80d41292c1f6db3176
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-30 14:04:52 +0300

    Settings: Theme tab added.

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -5,11 +5,10 @@ import { SETTINGS_DETAIL_TABS } from '../../enum/SETTINGS_DETAIL_TABS';
 import { General } from './General/General';
 import { Privacy } from './Privacy/Privacy';
 import './Settings.scss';
+import { Theme } from './Theme/Theme';
 
 export const Settings = (): JSX.Element => {
-  const [selectedTab, setSelectedTab] = useState<SETTINGS_DETAIL_TABS>(
-    SETTINGS_DETAIL_TABS.GENERAL,
-  );
+  const [selectedTab, setSelectedTab] = useState<SETTINGS_DETAIL_TABS>(SETTINGS_DETAIL_TABS.GENERAL);
 
   document.title = ROUTE_PATH_TITLE.SETTINGS;
 
@@ -22,7 +21,7 @@ export const Settings = (): JSX.Element => {
         return <Privacy />;
         break;
       case SETTINGS_DETAIL_TABS.THEME:
-        return <div></div>;
+        return <Theme />;
         break;
       case SETTINGS_DETAIL_TABS.ADVANCED:
         return <div></div>;
