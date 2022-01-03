a5ffe3dcfcdfcce5aa04b685de44ae06341c238f
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-03 19:11:44 +0300

    General: CustomPopover added

--- src/pages/Settings/General/General.tsx
+++ src/pages/Settings/General/General.tsx
@@ -5,6 +5,7 @@ import SettingsContext from '../../../context/SettingsContext';
 import { CheckBoxGroup } from '../../../components/CheckBoxGroup/CheckBoxGroup';
 import { StripedRadioButton } from '../../../components/StripedRadioButton/StripedRadioButton';
 import { ToggleSwitch } from '../../../components/ToggleSwitch/ToggleSwitch';
+import { CustomPopover } from '../../../components/CustomPopover/CustomPopover';
 import { PREFERRED_UNIT } from '../../../enum/PREFERRED_UNIT';
 import { preferredUnitList, preferredUnitOptions, SlippageFeeList, slippageFeeOptions } from './utils';
 import info from '../../../images/info2.png';
@@ -20,7 +21,13 @@ export const General = (): JSX.Element => {
       <div className="general-item">
         <div className="general-item-first-head">
           <span className="general-title">Slippage Tolerance</span>
-          <img className="general-icon" src={info} alt="info" />
+          <CustomPopover
+            placement="autoHorizontal"
+            title="Slippage Tolerance"
+            content="Your transaction will revert if the price changes unfavorably by more than this percentage."
+          >
+            <img className="general-icon" src={info} alt="info" />
+          </CustomPopover>
         </div>
         <StripedRadioButton
           options={slippageFeeOptions}
@@ -41,7 +48,13 @@ export const General = (): JSX.Element => {
       <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Preferred Unit</span>
-          <img className="general-icon" src={info} alt="info" />
+          <CustomPopover
+            placement="autoHorizontal"
+            title="Preferred Unit"
+            content="Your metric system of denominations used as units of Bitcoin. The smallest denomination base unit is sats."
+          >
+            <img className="general-icon" src={info} alt="info" />
+          </CustomPopover>
         </div>
 
         <CheckBoxGroup
@@ -66,14 +79,26 @@ export const General = (): JSX.Element => {
       <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Push Notifications</span>
-          <img className="general-icon" src={info} alt="info" />
+          <CustomPopover
+            placement="autoHorizontal"
+            title="Push Notifications"
+            content="Receive pop-up messages on your transaction status."
+          >
+            <img className="general-icon" src={info} alt="info" />
+          </CustomPopover>
           <ToggleSwitch onChange={(checked) => setPushNotificationsSwitch(checked)} checked={pushNotificationsSwitch} />
         </div>
       </div>
       <div className="general-item">
         <div className="general-item-head">
           <span className="general-title">Liquid Taxi</span>
-          <img className="general-icon" src={info} alt="info" />
+          <CustomPopover
+            placement="autoHorizontal"
+            title="Liquid Taxi"
+            content="Swap your Liquid assets without the need to pay fees in L-BTC."
+          >
+            <img className="general-icon" src={info} alt="info" />
+          </CustomPopover>
           <ToggleSwitch onChange={(checked) => setLiquidTaxiSwitch(checked)} checked={liquidTaxiSwitch} />
         </div>
       </div>
