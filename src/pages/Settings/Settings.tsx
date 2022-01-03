21206b1cdedb0f1a9eedb29252ccc86d8d52913b
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-03 19:17:55 +0300

    Settings: Advanced component added

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { TabMenu } from '../../components/TabMenu/TabMenu';
 import { ROUTE_PATH_TITLE } from '../../enum/ROUTE_PATH.TITLE';
 import { SETTINGS_DETAIL_TABS } from '../../enum/SETTINGS_DETAIL_TABS';
+import { Advanced } from './Advanced/Advanced';
 import { General } from './General/General';
 import { Privacy } from './Privacy/Privacy';
-import './Settings.scss';
 import { Theme } from './Theme/Theme';
+import './Settings.scss';
 
 export const Settings = (): JSX.Element => {
   const [selectedTab, setSelectedTab] = useState<SETTINGS_DETAIL_TABS>(SETTINGS_DETAIL_TABS.GENERAL);
@@ -24,7 +25,7 @@ export const Settings = (): JSX.Element => {
         return <Theme />;
         break;
       case SETTINGS_DETAIL_TABS.ADVANCED:
-        return <div></div>;
+        return <Advanced />;
         break;
       default:
         return;
