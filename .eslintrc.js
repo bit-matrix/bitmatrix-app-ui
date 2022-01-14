eda4daf645f9fe71d4eb069edc91b697ce0338ad
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-14 16:12:41 +0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   root: true,
   env: {
     node: true,
