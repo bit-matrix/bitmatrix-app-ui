c38531a0318855179e24b28e16706328c9a2dd7a
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-18 17:59:46 +0300

    PegIn page

---
+++ src/pages/Factory/PegIn/PegIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PegIn.scss';
+
+export const PegIn = (): JSX.Element => {
+  return (
+    <div className="peg-in-content">
+      <span className="no-peg-in-text">There are no peg in yet.</span>
+    </div>
+  );
+};
