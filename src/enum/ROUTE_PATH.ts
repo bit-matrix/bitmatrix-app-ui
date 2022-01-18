067c9b98fcb0284387fda19e6a5a04cab7cc5c34
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-18 18:01:22 +0300

    ROUTE_PATH: ISSUE_TOKEN

--- src/enum/ROUTE_PATH.ts
+++ src/enum/ROUTE_PATH.ts
@@ -8,8 +8,6 @@ export enum ROUTE_PATH {
   ADD_LIQUIDTY = '/pool/:id/add-liquidity',
   REMOVE_LIQUIDITY = '/pool/:id/remove-liquidity',
   FACTORY = '/factory',
-  ISSUANCE = '/factory/issuance',
-  PEG_IN = '/factory/peg-in',
-  PEG_OUT = '/factory/peg-out',
+  ISSUE_TOKEN = '/factory/issue-token',
   NOT_FOUND = '*',
 }
