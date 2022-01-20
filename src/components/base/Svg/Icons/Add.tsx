4d20e171110eb0c65c2e80117f0b4fdac4461433
afarukcali <afarukcali@gmail.com>
2022-01-20 15:57:53 +0300

    Icons component added.

---
+++ src/components/base/Svg/Icons/Add.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Svg from '../Svg';
+import { IconSvg } from './IconSvg';
+
+const Add: React.FC<IconSvg> = ({ fill, bgColor, size }) => (
+  <Svg fill={fill} bgColor={bgColor} size={size} path="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+);
+
+export default Add;
