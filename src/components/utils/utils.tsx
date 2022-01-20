51bcb4778c538b04d4c531f3d315556fb92ca539
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-20 17:35:25 +0300

    utils: notify func placement default value

--- src/components/utils/utils.tsx
+++ src/components/utils/utils.tsx
@@ -3,6 +3,11 @@ import { MessageType } from 'rsuite/esm/Notification/Notification';
 import { PlacementType } from 'rsuite/esm/toaster/ToastContainer';
 import { CustomNotify } from '../CustomNotify/CustomNotify';
 
-export const notify = (content: string, header?: string, type?: MessageType, placement?: PlacementType): string => {
+export const notify = (
+  content: string,
+  header?: string,
+  type?: MessageType,
+  placement: PlacementType = 'topEnd',
+): string => {
   return toaster.push(<CustomNotify header={header} content={content} type={type} />, { placement });
 };
