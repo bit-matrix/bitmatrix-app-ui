fa55972c870f1c38d9b0152b3f22202c6f1bad93
afarukcali <afarukcali@gmail.com>
2022-01-26 00:59:00 +0300

    InfoCard redesign.

--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -20,61 +20,59 @@ export const InfoCard: React.FC = () => {
   const message = (cs: CommitmentStore): JSX.Element | undefined => {
     let messageBody: JSX.Element | undefined;
 
-    if (!cs.completed) {
+    if (cs.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN || cs.method === CALL_METHOD.SWAP_TOKEN_FOR_QUOTE) {
       messageBody = (
         <>
-          <div className="info-card-item-icon "></div>
-          <div
-            className="explorer-div"
-            onClick={() => window.open(`https://blockstream.info/liquidtestnet/tx/${cs.poolTxId || cs.txId}`, '_blank')}
-          >
-            View in Block Explorer <ExportIcon fill="#575757" width="1.5rem" height="1.5rem" />
+          <div className="info-card-item-icon">
+            <ExchangeIcon width="1rem" height="1rem" />
+          </div>
+          <div>
+            Swap {cs.quoteAmount / PREFERRED_UNIT_VALUE.LBTC} {cs.quoteAsset} for {cs.tokenAsset} (min{' '}
+            {cs.tokenAmount / PREFERRED_UNIT_VALUE.LBTC})
           </div>
         </>
       );
-    } else {
-      if (cs.method === CALL_METHOD.SWAP_QUOTE_FOR_TOKEN || cs.method === CALL_METHOD.SWAP_TOKEN_FOR_QUOTE) {
-        messageBody = (
-          <>
-            <div className="info-card-item-icon ">
-              <ExchangeIcon width="1rem" height="1rem" />
-            </div>
-            <div>
-              Swap {cs.quoteAmount / PREFERRED_UNIT_VALUE.LBTC} {cs.quoteAsset} for {cs.tokenAsset} (min{' '}
-              {cs.tokenAmount / PREFERRED_UNIT_VALUE.LBTC})
-            </div>
-          </>
-        );
-      }
+    }
 
-      if (cs.method === CALL_METHOD.ADD_LIQUIDITY) {
-        messageBody = (
-          <>
-            <img className="info-card-item-icon-2" src={liqadd} alt="" />
-            <div>
-              Add {cs.quoteAmount} {cs.quoteAsset} and&nbsp;
-              {cs.tokenAmount} {cs.tokenAsset}
-            </div>
-          </>
-        );
-      }
+    if (cs.method === CALL_METHOD.ADD_LIQUIDITY) {
+      messageBody = (
+        <>
+          <img className="info-card-item-icon-2" src={liqadd} alt="" />
+          <div>
+            Add {cs.quoteAmount} {cs.quoteAsset} and&nbsp;
+            {cs.tokenAmount} {cs.tokenAsset}
+          </div>
+        </>
+      );
+    }
 
-      if (cs.method === CALL_METHOD.REMOVE_LIQUIDITY) {
-        messageBody = (
-          <>
-            <img className="info-card-item-icon-2" src={liqremove} alt="" />
-            <div>
-              Remove {cs.quoteAmount} {cs.quoteAsset} and&nbsp;
-              {cs.tokenAmount} {cs.tokenAsset}
-            </div>
-          </>
-        );
-      }
+    if (cs.method === CALL_METHOD.REMOVE_LIQUIDITY) {
+      messageBody = (
+        <>
+          <img className="info-card-item-icon-2" src={liqremove} alt="" />
+          <div>
+            Remove {cs.quoteAmount} {cs.quoteAsset} and&nbsp;
+            {cs.tokenAmount} {cs.tokenAsset}
+          </div>
+        </>
+      );
     }
 
     return (
       <div key={cs.txId} className="info-card-item">
         {messageBody}
+        {!cs.completed && (
+          <>
+            <div
+              className="explorer-div"
+              onClick={() =>
+                window.open(`https://blockstream.info/liquidtestnet/tx/${cs.poolTxId || cs.txId}`, '_blank')
+              }
+            >
+              View in Block Explorer <ExportIcon fill="#575757" width="1.5rem" height="1.5rem" />
+            </div>
+          </>
+        )}
         {cs.completed === false ? (
           <Loading width="1.5rem" height="1.5rem" />
         ) : (
