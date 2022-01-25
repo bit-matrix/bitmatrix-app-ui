5f583ee74fc0904ca82bb3b943df62372460a6a8
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-25 23:40:38 +0300

    ArrowDown,ArrowUp: default fill value removed

--- src/components/base/Svg/Icons/ArrowDown.tsx
+++ src/components/base/Svg/Icons/ArrowDown.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Svg from '../Svg';
 import { IconSvg } from './IconSvg';
 
-const ArrowDownIcon: React.FC<IconSvg> = ({ fill = '#ff0000', bgColor, width = '0.85rem', height = '0.85rem' }) => (
+const ArrowDownIcon: React.FC<IconSvg> = ({ fill, bgColor, width = '0.85rem', height = '0.85rem' }) => (
   <Svg
     fill={fill}
     bgColor={bgColor}
