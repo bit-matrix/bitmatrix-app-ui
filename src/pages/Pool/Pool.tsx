cf8e760c24f7d612f1e627c8799d71fcccc6e21c
huseyinsaritas <hsynsrtss@gmail.com>
2022-02-18 02:08:40 +0300

    Pool:  hooks context added.

--- src/pages/Pool/Pool.tsx
+++ src/pages/Pool/Pool.tsx
@@ -1,24 +1,22 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { usePoolContext } from '../../context';
 import { PoolManagement } from '../../components/PoolManagement/PoolManagement';
 import { ROUTE_PATH_TITLE } from '../../enum/ROUTE_PATH.TITLE';
-import SettingsContext from '../../context/SettingsContext';
 import { useHistory } from 'react-router-dom';
 import { ROUTE_PATH } from '../../enum/ROUTE_PATH';
 import './Pool.scss';
 
 export const Pool = (): JSX.Element => {
-  const { payloadData } = useContext(SettingsContext);
+  const { pools } = usePoolContext();
+
   const history = useHistory();
 
   document.title = ROUTE_PATH_TITLE.POOL;
 
-  if (payloadData.pools && payloadData.pools.length > 0) {
+  if (pools && pools.length > 0) {
     return (
       <div className="pool-main-div">
-        <PoolManagement
-          pools={payloadData.pools}
-          onClick={(poolId: string) => history.push(ROUTE_PATH.POOL + '/' + poolId)}
-        />
+        <PoolManagement pools={pools} onClick={(poolId: string) => history.push(ROUTE_PATH.POOL + '/' + poolId)} />
       </div>
     );
   }
