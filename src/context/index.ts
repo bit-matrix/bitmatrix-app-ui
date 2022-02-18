af14f8f0284de2d7c3f95ca26897ba341b0c5525
huseyinsaritas <hsynsrtss@gmail.com>
2022-02-18 03:31:31 +0300

    context index: providers export

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,21 +1,18 @@
-import { usePoolChartDataContext } from './poolChartData';
-import { poolChartDataReducer } from './poolChartData/reducer';
-import { usePoolConfigContext } from './poolConfig';
-import { poolConfigReducer } from './poolConfig/reducer';
-import { usePoolContext } from './pools';
-import { poolsReducer } from './pools/reducer';
-import { useSettingsContext } from './settings';
-import { settingsReducer } from './settings/reducer';
-import { combineComponents } from './utils/combineReducer';
-import { useWalletContext } from './wallet';
-import { walletReducer } from './wallet/reducer';
+import { PoolChartDataContextProvider, usePoolChartDataContext } from './poolChartData';
+import { PoolConfigContextProvider, usePoolConfigContext } from './poolConfig';
+import { PoolsContextProvider, usePoolContext } from './pools';
+import { SettingsContextProvider, useSettingsContext } from './settings';
+import { WalletContextProvider, useWalletContext } from './wallet';
 
-export const AppContextProvider = combineComponents(
-  poolsReducer as any,
-  poolChartDataReducer as any,
-  poolConfigReducer as any,
-  settingsReducer as any,
-  walletReducer as any,
-);
-
-export { usePoolContext, useWalletContext, usePoolConfigContext, usePoolChartDataContext, useSettingsContext };
+export {
+  usePoolContext,
+  useWalletContext,
+  usePoolConfigContext,
+  usePoolChartDataContext,
+  useSettingsContext,
+  PoolChartDataContextProvider,
+  PoolConfigContextProvider,
+  PoolsContextProvider,
+  SettingsContextProvider,
+  WalletContextProvider,
+};
