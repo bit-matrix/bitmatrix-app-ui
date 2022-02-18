7026480b8fe78cb0c849284ecb8e1cc49e3cc712
huseyinsaritas <hsynsrtss@gmail.com>
2022-02-18 14:18:35 +0300

    index: AppContextProvider moved up to CustomProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { AppRouter } from './components/AppRouter/AppRouter';
-import { AppContextProvider } from './context/utils/combineProviders';
-import {
-  PoolChartDataContextProvider,
-  PoolConfigContextProvider,
-  PoolsContextProvider,
-  SettingsContextProvider,
-  WalletContextProvider,
-} from './context';
+
 import './App.scss';
 
 const App = (): JSX.Element => {
-  return (
-    <AppContextProvider
-      providers={[
-        PoolsContextProvider,
-        PoolChartDataContextProvider,
-        PoolConfigContextProvider,
-        SettingsContextProvider,
-        WalletContextProvider,
-      ]}
-    >
-      <AppRouter />
-    </AppContextProvider>
-  );
+  return <AppRouter />;
 };
 
 export default App;
