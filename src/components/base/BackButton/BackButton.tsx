bc4481c0e4e1b1979f7022d88fa56304153ed3df
huseyinsaritas <hsynsrtss@gmail.com>
2022-02-18 15:56:01 +0300

    BackButton: onClick,buttonText props added.

--- src/components/base/BackButton/BackButton.tsx
+++ src/components/base/BackButton/BackButton.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { Button } from 'rsuite';
 import BackIcon from '../Svg/Icons/Back';
 import './BackButton.scss';
 
-export const BackButton = (): JSX.Element => {
-  const history = useHistory();
+type Props = {
+  onClick: () => void;
+  buttonText: string;
+};
+
+export const BackButton: React.FC<Props> = ({ onClick, buttonText }): JSX.Element => {
   return (
-    <Button className="back-button" onClick={() => history.goBack()}>
+    <Button className="back-button" onClick={onClick}>
       <BackIcon width="2.25rem" height="2.25rem" fill="#adfbc4" />
-      <div className="back-button-text">L-BTC/USDT</div>
+      <div className="back-button-text">{buttonText}</div>
     </Button>
   );
 };
