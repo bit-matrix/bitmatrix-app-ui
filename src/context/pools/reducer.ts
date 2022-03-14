eb106840701553c7ff7b69d5aef488a4e59e770d
huseyinsaritas <hsynsrtss@gmail.com>
2022-03-14 17:15:12 +0300

    poolsReducer: SET_POOLS case

--- src/context/pools/reducer.ts
+++ src/context/pools/reducer.ts
@@ -5,9 +5,15 @@ import { SetPoolsAction, SET_POOLS } from './types';
 export const poolsReducer: Reducer<Pool[], SetPoolsAction> = (state: Pool[], action: SetPoolsAction): Pool[] => {
   switch (action.type) {
     case SET_POOLS:
-      const newState = [...action.payload];
-      return newState;
-
+      if (state.length > 0) {
+        const pools = [...state];
+        const newPools = action.payload;
+        const difference = pools.filter(({ id: id1 }) => !newPools.some(({ id: id2 }) => id2 === id1));
+        const newState = pools.concat(difference);
+        return newState;
+      } else {
+        return [...action.payload];
+      }
     default:
       return state;
   }
