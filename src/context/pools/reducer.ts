a18b392ac698fa5b619e4a553a01f29e1fe976a0
huseyinsaritas <hsynsrtss@gmail.com>
2022-03-14 16:36:06 +0300

    initialPoolState removed

--- src/context/pools/reducer.ts
+++ src/context/pools/reducer.ts
@@ -1,47 +1,12 @@
 import { Pool } from '@bitmatrix/models';
+import { Reducer } from 'react';
 import { SetPoolsAction, SET_POOLS } from './types';
 
-export const initialPoolState: Pool = {
-  id: '',
-  quote: {
-    ticker: '',
-    name: '',
-    asset: '',
-    value: '',
-  },
-  token: {
-    ticker: '',
-    name: '',
-    asset: '',
-    value: '',
-  },
-  lp: {
-    ticker: '',
-    name: '',
-    asset: '',
-    value: '',
-  },
-  initialTx: {
-    block_hash: '',
-    block_height: 0,
-    txid: '',
-  },
-  lastSyncedBlock: { block_hash: '', block_height: 0 },
-  bestBlockHeight: 0,
-  synced: false,
-  unspentTx: {
-    block_hash: '',
-    block_height: 0,
-    txid: '',
-  },
-  lastSentPtx: '',
-  active: false,
-};
-
-export const poolsReducer = (state: Pool[], action: SetPoolsAction): Pool[] => {
+export const poolsReducer: Reducer<Pool[], SetPoolsAction> = (state: Pool[], action: SetPoolsAction): Pool[] => {
   switch (action.type) {
     case SET_POOLS:
-      return { ...state, ...action.payload };
+      const newState = [...action.payload];
+      return newState;
 
     default:
       return state;
