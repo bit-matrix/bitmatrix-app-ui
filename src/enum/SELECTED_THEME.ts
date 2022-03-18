72af71e92c9ccf811b48b95822dde6e209c93071
huseyinsaritas <hsynsrtss@gmail.com>
2022-03-18 16:44:30 +0300

    enum SELECTED_THEME

---
+++ src/enum/SELECTED_THEME.ts
@@ -0,0 +1,10 @@
+export enum SELECTED_THEME {
+  GRAY = 'gray',
+  WHITE = 'white',
+  YELLOW = 'yellow',
+  RED = 'red',
+  BLUE = 'blue',
+  PINK = 'pink',
+  TURQUOISE = 'turquoise',
+  NEON = 'neon',
+}
