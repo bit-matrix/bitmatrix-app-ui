8ff38305d38e63e1ce6f5524aea2459cb10538ea
huseyinsaritas <hsynsrtss@gmail.com>
2022-03-21 22:07:37 +0300

    Theme: themeOnClick func theme saved in context and read by context

--- src/pages/Settings/Theme/Theme.tsx
+++ src/pages/Settings/Theme/Theme.tsx
@@ -1,43 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { CustomPopover } from '../../../components/CustomPopover/CustomPopover';
+import SettingsContext from '../../../context/SettingsContext';
+import SETTINGS_ACTION_TYPES from '../../../context/SETTINGS_ACTION_TYPES';
 import { SELECTED_THEME } from '../../../enum/SELECTED_THEME';
-import { useLocalStorage } from '../../../hooks/useLocalStorage';
 import info from '../../../images/info2.png';
 // import exclusiveIcon from '../../../images/mtx.png';
 import './Theme.scss';
 
 export const Theme = (): JSX.Element => {
-  const [selected, setSelected] = useState<SELECTED_THEME>();
-  const { getLocalData, setLocalData } = useLocalStorage<SELECTED_THEME>('theme');
-
-  useEffect(() => {
-    const storedTheme: SELECTED_THEME | undefined = getLocalData();
-    if (storedTheme) {
-      setSelected(storedTheme);
-    } else {
-      setSelected(SELECTED_THEME.NEON);
-    }
-  }, []);
-
-  const setThemeBeforeUnload = () => {
-    if (selected) setLocalData(selected);
-  };
-
-  const setupBeforeUnloadListener = useCallback(() => {
-    window.addEventListener('beforeunload', (ev) => {
-      ev.preventDefault();
-      return setThemeBeforeUnload();
-    });
-  }, [setThemeBeforeUnload]);
-
-  useEffect(() => {
-    // Activate the event listener
-    setupBeforeUnloadListener();
-  }, [setupBeforeUnloadListener]);
+  const { dispatch, payloadData } = useContext(SettingsContext);
 
   const themeOnClick = (selectedTheme: SELECTED_THEME) => {
-    setSelected(selectedTheme);
-    document.documentElement.setAttribute('theme', selectedTheme);
+    dispatch({
+      type: SETTINGS_ACTION_TYPES.SET_THEME,
+      payload: {
+        ...payloadData,
+        theme: selectedTheme,
+      },
+    });
   };
 
   return (
@@ -55,31 +35,33 @@ export const Theme = (): JSX.Element => {
         </div>
         <div className="theme-item-content">
           <div
-            className={`theme-tag neon-theme ${selected === SELECTED_THEME.NEON && 'theme-selected'}`}
+            className={`theme-tag neon-theme ${payloadData.theme === SELECTED_THEME.NEON && 'theme-selected'}`}
             onClick={() => themeOnClick(SELECTED_THEME.NEON)}
           />
           <div
-            className={`theme-tag white-theme ${selected === SELECTED_THEME.WHITE && 'theme-selected'}`}
+            className={`theme-tag white-theme ${payloadData.theme === SELECTED_THEME.WHITE && 'theme-selected'}`}
             onClick={() => themeOnClick(SELECTED_THEME.WHITE)}
           />
           <div
-            className={`theme-tag yellow-theme ${selected === SELECTED_THEME.YELLOW && 'theme-selected'}`}
+            className={`theme-tag yellow-theme ${payloadData.theme === SELECTED_THEME.YELLOW && 'theme-selected'}`}
             onClick={() => themeOnClick(SELECTED_THEME.YELLOW)}
           />
           <div
-            className={`theme-tag red-theme ${selected === SELECTED_THEME.RED && 'theme-selected'}`}
+            className={`theme-tag red-theme ${payloadData.theme === SELECTED_THEME.RED && 'theme-selected'}`}
             onClick={() => themeOnClick(SELECTED_THEME.RED)}
           />
           <div
-            className={`theme-tag blue-theme ${selected === SELECTED_THEME.BLUE && 'theme-selected'}`}
+            className={`theme-tag blue-theme ${payloadData.theme === SELECTED_THEME.BLUE && 'theme-selected'}`}
             onClick={() => themeOnClick(SELECTED_THEME.BLUE)}
           />
           <div
-            className={`theme-tag pink-theme ${selected === SELECTED_THEME.PINK && 'theme-selected'}`}
+            className={`theme-tag pink-theme ${payloadData.theme === SELECTED_THEME.PINK && 'theme-selected'}`}
             onClick={() => themeOnClick(SELECTED_THEME.PINK)}
           />
           <div
-            className={`theme-tag turquoise-theme ${selected === SELECTED_THEME.TURQUOISE && 'theme-selected'}`}
+            className={`theme-tag turquoise-theme ${
+              payloadData.theme === SELECTED_THEME.TURQUOISE && 'theme-selected'
+            }`}
             onClick={() => themeOnClick(SELECTED_THEME.TURQUOISE)}
           />
         </div>
