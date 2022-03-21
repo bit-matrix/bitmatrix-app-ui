37f9b4717a645a93f657062ef7839b371b2d6cbc
huseyinsaritas <hsynsrtss@gmail.com>
2022-03-21 22:03:02 +0300

    PayloadData: theme

--- src/context/PayloadData.ts
+++ src/context/PayloadData.ts
@@ -2,6 +2,7 @@ import { PREFERRED_UNIT } from '../enum/PREFERRED_UNIT';
 import { Pool, BmConfig, BmChart } from '@bitmatrix/models';
 import { IWallet } from '../lib/wallet/IWallet';
 import { Balance } from 'marina-provider';
+import { SELECTED_THEME } from '../enum/SELECTED_THEME';
 
 interface PayloadData {
   slippage: number;
@@ -10,6 +11,7 @@ interface PayloadData {
   pool_config?: BmConfig;
   wallet?: { marina: IWallet; isEnabled: boolean; balances: Balance[] };
   pool_chart_data?: BmChart[];
+  theme: SELECTED_THEME;
 }
 
 export default PayloadData;
