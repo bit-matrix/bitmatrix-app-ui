40ef1b237558e8f8915c8e45ccd616087f36981b
huseyinsaritas <hsynsrtss@gmail.com>
2022-04-11 23:04:16 +0300

    poolChartData,pools reducer deepCopy

--- src/context/poolChartData/reducer.ts
+++ src/context/poolChartData/reducer.ts
@@ -1,20 +1,24 @@
 import { BmChart } from '@bitmatrix/models';
+import { deepCopy } from '../../helper';
 import { SetPoolChartDataAction, SET_POOL_CHART_DATA } from './types';
 
 export const poolChartDataReducer = (state: BmChart[], action: SetPoolChartDataAction): BmChart[] => {
   switch (action.type) {
     case SET_POOL_CHART_DATA:
-      if (state.length > 0) {
-        const poolChartData = [...state];
-        const newPoolChartData = action.payload;
-        const difference = poolChartData.filter(
-          ({ ptxid: id1 }) => !newPoolChartData.some(({ ptxid: id2 }) => id2 === id1),
-        );
-        const newState = poolChartData.concat(difference);
-        return newState;
-      } else {
-        return [...action.payload];
-      }
+      // if (state.length > 0) {
+      //   const poolChartData = [...state];
+      //   const newPoolChartData = action.payload;
+      //   const difference = poolChartData.filter(
+      //     ({ ptxid: id1 }) => !newPoolChartData.some(({ ptxid: id2 }) => id2 === id1),
+      //   );
+      //   const newState = poolChartData.concat(difference);
+      //   return newState;
+      // } else {
+      //   return [...action.payload];
+      // }
+
+      const poolChartData = deepCopy(action.payload);
+      return poolChartData;
 
     default:
       return state;
--- src/context/pools/reducer.ts
+++ src/context/pools/reducer.ts
@@ -1,19 +1,22 @@
 import { Pool } from '@bitmatrix/models';
 import { Reducer } from 'react';
+import { deepCopy } from '../../helper';
 import { SetPoolsAction, SET_POOLS } from './types';
 
 export const poolsReducer: Reducer<Pool[], SetPoolsAction> = (state: Pool[], action: SetPoolsAction): Pool[] => {
   switch (action.type) {
     case SET_POOLS:
-      if (state.length > 0) {
-        const pools = [...state];
-        const newPools = action.payload;
-        const difference = pools.filter(({ id: id1 }) => !newPools.some(({ id: id2 }) => id2 === id1));
-        const newState = pools.concat(difference);
-        return newState;
-      } else {
-        return [...action.payload];
-      }
+      // if (state.length > 0) {
+      //   const pools = [...state];
+      //   const newPools = action.payload;
+      //   const difference = pools.filter(({ id: id1 }) => !newPools.some(({ id: id2 }) => id2 === id1));
+      //   const newState = pools.concat(difference);
+      //   return newState;
+      // } else {
+      //   return [...action.payload];
+      // }
+      const pools = deepCopy(action.payload);
+      return pools;
     default:
       return state;
   }
