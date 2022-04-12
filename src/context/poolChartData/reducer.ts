7fea749f49c7d75d313efa2b251e379a45ddb1d1
huseyinsaritas <hsynsrtss@gmail.com>
2022-04-12 15:33:26 +0300

    Pool Chart Data reducer if data lenghts are same return old data

--- src/context/poolChartData/reducer.ts
+++ src/context/poolChartData/reducer.ts
@@ -5,20 +5,13 @@ import { SetPoolChartDataAction, SET_POOL_CHART_DATA } from './types';
 export const poolChartDataReducer = (state: BmChart[], action: SetPoolChartDataAction): BmChart[] => {
   switch (action.type) {
     case SET_POOL_CHART_DATA:
-      // if (state.length > 0) {
-      //   const poolChartData = [...state];
-      //   const newPoolChartData = action.payload;
-      //   const difference = poolChartData.filter(
-      //     ({ ptxid: id1 }) => !newPoolChartData.some(({ ptxid: id2 }) => id2 === id1),
-      //   );
-      //   const newState = poolChartData.concat(difference);
-      //   return newState;
-      // } else {
-      //   return [...action.payload];
-      // }
-
       const poolChartData = deepCopy(action.payload);
-      return poolChartData;
+
+      if (state.length !== action.payload.length) {
+        return poolChartData;
+      } else {
+        return state;
+      }
 
     default:
       return state;
