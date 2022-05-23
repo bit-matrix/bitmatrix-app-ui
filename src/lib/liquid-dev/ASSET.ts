1564a49b4267c731576e4524791bef5ab8f0ccbf
huseyinsartas <hsynsrtss@gmail.com>
2022-05-23 18:00:39 +0300

    ASSET consts

---
+++ src/lib/liquid-dev/ASSET.ts
@@ -0,0 +1,16 @@
+import SWAP_ASSET from '../../enum/SWAP_ASSET';
+import { Asset } from '../../model/Asset';
+
+export const lbtcAsset: Asset = {
+  assetHash: '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49',
+  name: 'Liquid Bitcoin',
+  precision: 8,
+  ticker: SWAP_ASSET.LBTC,
+};
+
+export const usdtAsset: Asset = {
+  assetHash: 'f3d1ec678811398cd2ae277cbe3849c6f6dbd72c74bc542f7c4b11ff0e820958',
+  name: 'Tether USD',
+  precision: 8,
+  ticker: SWAP_ASSET.USDT,
+};
