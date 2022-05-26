c8cc22001dd4acc69fd3afd4071ec7cdf48772c4
huseyinsartas <hsynsrtss@gmail.com>
2022-05-26 18:09:39 +0300

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MediumIcon from '../base/Svg/Icons/Medium';
+import TwitterIcon from '../base/Svg/Icons/Twitter';
+import TelegramIcon from '../base/Svg/Icons/Telegram';
+import GithubIcon from '../base/Svg/Icons/Github';
+import './Footer.scss';
+
+export const Footer = (): JSX.Element => {
+  return (
+    <div className="swap-page-footer">
+      <div className="swap-page-footer-icons">
+        <a href="https://medium.com/bit-matrix" target="_blank" className="swap-page-footer-icon-item">
+          <MediumIcon width="2rem" height="2rem" />
+        </a>
+        <a href="https://twitter.com/bitmatrix_" target="_blank" className="swap-page-footer-icon-item">
+          <TwitterIcon width="2rem" height="2rem" />
+        </a>
+        <a href="https://t.me/bitmatrix_community" target="_blank" className="swap-page-footer-icon-item">
+          <TelegramIcon width="2rem" height="2rem" />
+        </a>
+        <a href="https://github.com/bit-matrix" target="_blank" className="swap-page-footer-icon-item">
+          <GithubIcon width="2rem" height="2rem" />
+        </a>
+      </div>
+    </div>
+  );
+};
