251db6f20c1721a430dcb72d7b7c9ffba3a2d124
huseyinsartas <hsynsrtss@gmail.com>
2022-06-10 17:14:44 +0300

    poolConfig context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,12 +1,15 @@
 import { PoolsContextProvider, usePoolContext } from './pools';
 import { SettingsContextProvider, useSettingsContext } from './settings';
 import { WalletContextProvider, useWalletContext } from './wallet';
+import { PoolConfigContextProvider, usePoolConfigContext } from './poolConfig';
 
 export {
   usePoolContext,
-  useWalletContext,
   useSettingsContext,
+  useWalletContext,
+  usePoolConfigContext,
   PoolsContextProvider,
   SettingsContextProvider,
   WalletContextProvider,
+  PoolConfigContextProvider,
 };
---
+++ src/context/poolConfig/reducer.ts
@@ -0,0 +1,45 @@
+import { BmConfig } from '@bitmatrix/models';
+import { Reducer } from 'react';
+import { SetPoolConfigAction, SET_POOL_CONFIG } from './types';
+
+export const initialPoolConfigState: BmConfig = {
+  id: '',
+  minRemainingSupply: 0,
+  minTokenValue: 0,
+  baseFee: { number: 0, hex: '' },
+  serviceFee: { number: 0, hex: '' },
+  commitmentTxFee: { number: 0, hex: '' },
+  defaultOrderingFee: { number: 0, hex: '' },
+  innerPublicKey: '',
+  recipientValueMinus: 0,
+  holderCovenant: {
+    scriptpubkey: {
+      main: '',
+      token: '',
+      lp: '',
+    },
+    controlBlockPrefix: {
+      main: '',
+      token: '',
+      lp: '',
+    },
+  },
+  mainCovenantScript: [],
+  maxLeaf: 0,
+};
+
+export const poolConfigReducer: Reducer<BmConfig, SetPoolConfigAction> = (
+  state: BmConfig,
+  action: SetPoolConfigAction,
+): BmConfig => {
+  switch (action.type) {
+    case SET_POOL_CONFIG:
+      return {
+        ...state,
+        ...action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
