ff4be1bf23c89d30046570746342cafa8e428a01
huseyinsartas <hsynsrtss@gmail.com>
2022-06-21 15:25:04 +0300

    useSocket

--- src/socket/useSocket.ts
+++ src/socket/useSocket.ts
@@ -5,18 +5,9 @@ import { Pool } from '@bitmatrix/models';
 const socketServerUrl = '//127.0.0.1:4499';
 
 export const useSocket = () => {
-  // const [ws, setWs] = useState<WebSocket>();
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const [pools, setPools] = useState<Pool[]>();
   const [poolsLoading, setPoolsLoading] = useState<boolean>(false);
-
-  const socket = io(socketServerUrl);
-
-  // useEffect(() => {
-  //   if (ws && ws.readyState) {
-  //     setIsConnected(ws.readyState === 1);
-  //   }
-  // }, [ws, ws?.readyState]);
 
   const onPools = useCallback((pools: Pool[]) => {
     setPools(pools);
@@ -24,25 +15,7 @@ export const useSocket = () => {
   }, []);
 
   useEffect(() => {
-    // Open connection
-    // const newWs = new WebSocket(WS_URL);
-    // newWs.onopen = () => {
-    //   setWs(newWs);
-    //   setIsConnected(true);
-    //   console.log('Connection opened.');
-    // };
-
-    // newWs.onerror = (err) => {
-    //   console.error('Socket encountered error: ', err, 'Closing socket');
-    //   newWs.close();
-    //   setPools([]);
-    // };
-
-    // newWs.onclose = (event) => {
-    //   setPools([]);
-    //   setIsConnected(false);
-    //   console.log(`Socket is closed.`, event.reason);
-    // };
+    const socket = io(socketServerUrl);
 
     socket.on('connect', () => {
       console.log('connect');
@@ -54,45 +27,9 @@ export const useSocket = () => {
       setIsConnected(false);
     });
 
-    socket.on('ping', () => {
-      console.log('ping');
-      socket.emit('pong');
-    });
-
     socket.on('pools', (data) => {
-      onPools(data);
+      if (data) onPools(data);
     });
-
-    // socket.on('message', (event) => {
-    //   try {
-    //     console.log('event', event);
-    //     const data = JSON.parse(event.data);
-    //     onPools(data);
-    //   } catch {
-    //     console.error(`JSON parse error onmessage: ${event.data}`);
-    //   }
-    // });
-
-    // newWs.onmessage = (event) => {
-    //   try {
-    //     if (event.data === 'ping') {
-    //       newWs.send('pong');
-    //     } else {
-    //       const data = JSON.parse(event.data);
-    //       onPools(data);
-    //     }
-    //   } catch {
-    //     console.error(`JSON parse error onmessage: ${event.data}`);
-    //   }
-    // };
-
-    // Close connection
-    // return () => {
-    //   if (ws) {
-    //     ws.close();
-    //     setIsConnected(false);
-    //   }
-    // };
 
     return () => {
       socket.off('connect');
@@ -104,7 +41,6 @@ export const useSocket = () => {
   }, []);
 
   return {
-    // ws,
     isConnected,
     poolsLoading,
     pools,
