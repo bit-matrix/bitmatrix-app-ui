32ca936f41aeec441dc87e8b57a2e00b0e5fcc82
huseyinsartas <hsynsrtss@gmail.com>
2022-07-20 17:28:57 +0300

    useTxStatusSocket

---
+++ src/hooks/useTxStatusSocket.ts
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socketServerUrl = '//127.0.0.1:9901/';
+
+enum TX_STATUS {
+  PENDING,
+  WAITING_PTX,
+  WAITING_PTX_CONFIRM,
+  SUCCESS,
+  FAILED,
+}
+
+export type TxStatus = {
+  txId: string;
+  poolTxId: string;
+  status: TX_STATUS;
+  timestamp: number;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const useTxStatusSocket = (txIds: string[]) => {
+  const [isTxStatusConnected, setIsTxStatusConnected] = useState<boolean>(false);
+  const [txStatues, setTxStatues] = useState<TxStatus[]>();
+  const [txStatusLoading, setTxStatusLoading] = useState<boolean>(true);
+
+  const onTxStatusData = useCallback((txStatues: TxStatus[]) => {
+    setTxStatues(txStatues);
+    setTxStatusLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const socket = io(socketServerUrl);
+
+    socket.on('connect', () => {
+      console.log('connect txStatues');
+      setIsTxStatusConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnect txStatues');
+      setIsTxStatusConnected(false);
+    });
+
+    socket.emit('checkTxStatus', `${txIds[0]}`);
+
+    socket.on('checkTxStatusResponse', (data) => {
+      console.log('checkTxStatusResponse', data);
+      if (data) onTxStatusData(data);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+      setIsTxStatusConnected(false);
+      console.log('cleanup txStatues');
+    };
+  }, []);
+
+  return {
+    isTxStatusConnected,
+    txStatusLoading,
+    txStatues,
+  };
+};
