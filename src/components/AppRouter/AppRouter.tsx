3a7abe1d5c56aa8aee97c8861aa68e4ee5e31549
afarukcali <afarukcali@gmail.com>
2022-07-28 01:39:34 +0300

    pool last state.

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -138,13 +138,13 @@ export const AppRouter = (): JSX.Element => {
         ticker: 'tL-BTC',
         name: 'Liquid Bitcoin',
         asset: '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49',
-        value: '200000',
+        value: '209442',
       },
       token: {
         ticker: 'tL-USDt',
         name: 'Liquid Tether',
         asset: 'f3d1ec678811398cd2ae277cbe3849c6f6dbd72c74bc542f7c4b11ff0e820958',
-        value: '100000000000',
+        value: '95550000000',
       },
       lp: {
         ticker: 'fc65',
