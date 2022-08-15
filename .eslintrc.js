d513cb6552a9632bf854f2d04c8fd82f28b5d871
afarukcali <afarukcali@gmail.com>
2022-08-15 15:17:42 +0300

    eslint conf.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'react-app',
+    'react-app/jest',
+  ],
   root: true,
   env: {
     node: true,
