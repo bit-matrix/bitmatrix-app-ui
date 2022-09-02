1898e3a3eaec184103d42ad73b059a4199da99da
reyhanboydag <reyhanboydag@gmail.com>
2022-09-03 02:10:58 +0300

    env.ts updated

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,18 @@
 import { TESTNET_ASSET_ID } from './lib/liquid-dev/ASSET_ID';
 
+const base_host = process.env.UI_BASE_HOST || 'localhost';
+
+const api_port = process.env.UI_API_PORT || '8000';
+const api_host = process.env.UI_API_HOST || base_host;
+const api_url = process.env.UI_API_URL || `http://${api_host}:${api_port}`;
+
+const db_port = process.env.UI_DB_PORT || '8001';
+const db_host = process.env.UI_DB_HOST || base_host;
+const db_url = process.env.UI_DB_URL || `http://${db_host}:${db_port}`;
+
+export const API_SOCKET_SERVER_URL = api_url + '/';
+export const DB_SOCKET_SERVER_URL = db_url + '/';
+
 export const PAIR_1_LIST = [TESTNET_ASSET_ID.LBTC, TESTNET_ASSET_ID.USDT];
 
 export const LBTC_ASSET = '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49';
