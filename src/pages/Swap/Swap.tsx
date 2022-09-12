5eb029313cc1ab35be320265b6bb6a60ea04090b
huseyinsartas <hsynsrtss@gmail.com>
2022-09-12 15:23:36 +0300

    calcAmountPercent fix

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -218,24 +218,25 @@ export const Swap: React.FC<Props> = ({ checkTxStatusWithIds }): JSX.Element => 
         let quoteAmount = totalAmountInWallet;
 
         if (fromAsset.assetHash === lbtcAsset.assetHash) {
-          quoteAmount = quoteAmount - (newFromAmountPercent === FROM_AMOUNT_PERCENT.ALL ? totalFee : 0);
+          quoteAmount = quoteAmount - totalFee;
         }
+        if (quoteAmount > 0) {
+          const assetPrecision = getAssetPrecession(fromAsset, settingsContext.preferred_unit.text);
 
-        const assetPrecision = getAssetPrecession(fromAsset, settingsContext.preferred_unit.text);
+          if (newFromAmountPercent === FROM_AMOUNT_PERCENT.ALL) {
+            inputAmount = (quoteAmount / Math.pow(10, assetPrecision)).toString();
+          }
+          if (newFromAmountPercent === FROM_AMOUNT_PERCENT.HALF) {
+            const quoteAmountHalf = Math.ceil(quoteAmount / 2);
+            inputAmount = (quoteAmountHalf / Math.pow(10, assetPrecision)).toString();
+          }
+          if (newFromAmountPercent === FROM_AMOUNT_PERCENT.MIN) {
+            inputAmount = (poolConfigContext.minRemainingSupply / Math.pow(10, assetPrecision)).toString();
+          }
 
-        if (newFromAmountPercent === FROM_AMOUNT_PERCENT.ALL) {
-          inputAmount = (quoteAmount / Math.pow(10, assetPrecision)).toString();
+          setFromAmount(inputAmount);
+          setSelectedFromAmountPercent(newFromAmountPercent);
         }
-        if (newFromAmountPercent === FROM_AMOUNT_PERCENT.HALF) {
-          const quoteAmountHalf = Math.ceil(quoteAmount / 2);
-          inputAmount = (quoteAmountHalf / Math.pow(10, assetPrecision)).toString();
-        }
-        if (newFromAmountPercent === FROM_AMOUNT_PERCENT.MIN) {
-          inputAmount = (poolConfigContext.minRemainingSupply / Math.pow(10, assetPrecision)).toString();
-        }
-
-        setFromAmount(inputAmount);
-        setSelectedFromAmountPercent(newFromAmountPercent);
       }
     }
   };
