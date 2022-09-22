6265af10f06af979578d3fb6815b2098d38c1b7f
afarukcali <afarukcali@gmail.com>
2022-09-23 00:51:46 +0300

    create pool review.

--- src/pages/CreateNewPool/CreateNewPool.tsx
+++ src/pages/CreateNewPool/CreateNewPool.tsx
@@ -233,7 +233,7 @@ export const CreateNewPool: React.FC = () => {
             addressInformation.publicKey,
             1,
             pair1CoefficientCalculation(),
-            lbtcAsset.assetHash,
+            LBTC_ASSET.assetHash,
             lpFeeTier.index,
           );
 
